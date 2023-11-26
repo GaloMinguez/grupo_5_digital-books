2e60ad50ea72598344c670373cc788944a62074e
malnieva <malnieva@gmai.com>
2023-11-26 12:02:52 -0300

    agregando resolucion lista de productos correspondientes al sprint 4

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -4,6 +4,7 @@ const controller = {
     index: (req, res) => res.render('index'),
     register: (req, res) => res.render('../views/users/register'),
     login: (req, res) => res.render('../views/users/login'),
+    productList: (req, res) => res.render('../views/products/productList', { libros }),    
     productDetail: (req, res) => {
         const id = req.params.id;
         const getBook = libros.find(libro => libro.id == id);
